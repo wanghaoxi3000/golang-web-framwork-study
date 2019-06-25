0cea8fa265bd1b968f8fd359e1080e670a47364c
wanghaoxi <wanghaoxi3000@163.com>
2019-06-25 17:21:36 +0800

    添加 message 模块

---
+++ ziface/imessage.go
@@ -0,0 +1,11 @@
+package ziface
+
+//IMessage 将请求的消息封装到一个 Message 中
+type IMessage interface {
+	GetMsgID() uint32  //获取消息的ID
+	GetMsgLen() uint32 //获取消息的长度
+	GetData() []byte   //获取消息的内容
+	SetMsgID(uint32)   //设置消息的ID
+	SetData([]byte)    //设置消息的内容
+	SetDataLen(uint32) //设置消息的长度
+}
---
+++ znet/message.go
@@ -0,0 +1,38 @@
+package znet
+
+//Message 请求消息
+type Message struct {
+	ID      uint32 //消息的 ID
+	DataLen uint32 //消息的长度
+	Data    []byte //消息的内容
+}
+
+//GetMsgID 获取消息的ID
+func (m *Message) GetMsgID() uint32 {
+	return m.ID
+}
+
+//GetMsgLen 获取消息的长度
+func (m *Message) GetMsgLen() uint32 {
+	return m.DataLen
+}
+
+//GetData 获取消息的内容
+func (m *Message) GetData() []byte {
+	return m.Data
+}
+
+//SetMsgID 设置消息的ID
+func (m *Message) SetMsgID(id uint32) {
+	m.ID = id
+}
+
+//SetData 设置消息的内容
+func (m *Message) SetData(data []byte) {
+	m.Data = data
+}
+
+//SetDataLen 设置消息的长度
+func (m *Message) SetDataLen(len uint32) {
+	m.DataLen = len
+}
