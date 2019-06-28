dd08c9b9121c022de75e37e3b36b80cff7be00e7
wanghaoxi <wanghaoxi3000@163.com>
2019-06-28 11:19:35 +0800

    更新版本号

--- utils/globalobj.go
+++ utils/globalobj.go
@@ -44,7 +44,7 @@ func init() {
 	//如果配置文件没有加载，默认的值
 	GlobalObject = &GlobalObj{
 		Name:           "ZinxServerApp",
-		Version:        "V0.5",
+		Version:        "V0.6",
 		TCPPort:        8999,
 		Host:           "0.0.0.0",
 		MaxConn:        1000,
